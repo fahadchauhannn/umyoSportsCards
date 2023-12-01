0b3d5d1157a88082675c82efc139c7066c81eaef
devkhalil1337 <devkhalil1337@gmail.com>
2023-12-01 23:36:56 +0500

    Fixed build and title issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Title, Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projectx';
+  title = 'Umyo Sports Cards';
+  constructor(private titleService: Title, private metaService: Meta) {
+    this.titleService.setTitle(this.title)
+  }
 }
