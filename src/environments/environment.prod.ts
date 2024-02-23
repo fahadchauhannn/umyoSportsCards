0fe9803757e67ea3baff1e933f2c83e69060cda4
Saif Ali <58659225+saifxo@users.noreply.github.com>
2024-02-23 20:42:15 +0500

    truckers push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://prolivingbiz.com/api_umyocards/public/api/"
+  apiUrl: "https://umyotruckers.site/api_umyocards/public/api/"
 };
 
 
