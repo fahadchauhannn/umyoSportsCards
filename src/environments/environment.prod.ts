1ee7a0d240b60120aa348b36a3573ce3a76b53ee
Saif Ali <58659225+saifxo@users.noreply.github.com>
2024-06-04 00:53:32 +0500

    added header from desgin files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: "https://umyonba.site/api_umyocards/public/api/"
+  // apiUrl: "https://umyonba.site/api_umyocards/public/api/"
+  apiUrl: "http://127.0.0.1:8000/api/"
 };
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://umyonba.site/api_umyocards/public/api/"
+  apiUrl: "http://127.0.0.1:8000/api/"
+  // apiUrl: "https://umyonba.site/api_umyocards/public/api/"
+  
 };
 
 
