03721a8ba23f2b9a02ca9d9819287250c84a69cb
Saif Ali <58659225+saifxo@users.noreply.github.com>
2024-06-07 02:03:50 +0500

    added env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // apiUrl: "https://umyonba.site/api_umyocards/public/api/"
-  apiUrl: "http://127.0.0.1:8000/api/"
+  apiUrl: "https://lieutenantsacademydfw.org/api_umyocards/public/api/"
 };
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   // apiUrl: "http://127.0.0.1:8000/api/"
-  apiUrl: "https://umyoentertainment.site/api_umyocards/public/api/"
+  apiUrl: "https://lieutenantsacademydfw.org/api_umyocards/public/api/"
   
 };
 
