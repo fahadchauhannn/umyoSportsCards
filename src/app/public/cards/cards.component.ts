a1416888d96db0cf676755fccd94ff934e412627
Saif Ali <58659225+saifxo@users.noreply.github.com>
2024-06-11 03:46:35 +0500

    chagned url

--- src/app/public/cards/cards.component.ts
+++ src/app/public/cards/cards.component.ts
@@ -170,7 +170,7 @@ export class CardsComponent implements AfterViewInit{
                   alert("Failed to Verify Paypal Payment")
                   localStorage.removeItem("updatePaypalId");
                   localStorage.removeItem("updatedToken");
-                  window.location.href='https://umyomedical.site/'
+                  window.location.href='https://umyopd.site/'
               }
             );
           }
@@ -219,7 +219,7 @@ this.paymentForm = this.fb.group({
     
   }
   openShareDialog() {
-    const shareUrl = `https://umyomedical.site//cards/share-card/${this.shareCardId}`;
+    const shareUrl = `https://umyopd.site/cards/share-card/${this.shareCardId}`;
     const shareText = 'Check out my sports card!';
 
     // Open a new window for sharing
