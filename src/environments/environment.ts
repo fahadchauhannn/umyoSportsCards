fc27f68f006180daf125db051184de5420f77221
saifxo <saifii.7243@gmail.com>
2024-08-18 00:16:34 +0500

    union

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   
-  apiUrl: "https://umyoreligions.site/api_umyocards/public/api/"
+  apiUrl: "https://umyounions.site/api_umyocards/public/api/"
   
 };
 
