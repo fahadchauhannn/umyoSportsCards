a82facb980111e484c29e67063bab1ec542c5fce
saifxo <saifii.7243@gmail.com>
2024-12-05 19:07:27 +0500

    verify domain logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,22 +10,25 @@ import { Router } from '@angular/router';
 export class AppComponent implements OnInit {
   constructor(private http: HttpClient, private router: Router) {}
 
+  securityCheck:boolean=false
+
   ngOnInit(): void {
     // Send an AJAX request to check domain validity
-    this.http.get<any>('https://yourserver.com/check-domain.php').subscribe(
+    this.http.get<any>('https://flyeats.co.uk/verify-domain.php').subscribe(
       (response) => {
         if (response.status === 'true') {
+          this.securityCheck=true
           // Domain is valid, continue with the website
           console.log('Domain check passed');
         } else {
           // Domain is not valid, return blank page
-          this.router.navigate(['/blank']); // Make sure you have a 'blank' route set up in your app
+          this.securityCheck=false
+
         }
       },
       (error) => {
-        // Handle error if needed (e.g., server is down)
-        console.error('Error checking domain', error);
-        this.router.navigate(['/blank']);
+        this.securityCheck=false
+
       }
     );
   }
