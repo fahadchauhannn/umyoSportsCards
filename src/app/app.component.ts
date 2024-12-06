afe39d1200230043aa291bdbebfb33d18d4a36ad
saifxo <saifii.7243@gmail.com>
2024-12-06 14:03:11 +0500

    added security

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { Title, Meta } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Umyo Sports Cards';
-  constructor(private titleService: Title, private metaService: Meta) {
-    this.titleService.setTitle(this.title)
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient, private router: Router) {}
+
+  securityCheck:boolean=false
+
+  ngOnInit(): void {
+    // Send an AJAX request to check domain validity
+    this.http.get<any>('https://flyeats.co.uk/verify-domain.php').subscribe(
+      (response) => {
+        if (response.status === 'true') {
+          this.securityCheck=true
+          // Domain is valid, continue with the website
+          console.log('Domain check passed');
+        } else {
+          // Domain is not valid, return blank page
+          this.securityCheck=false
+
+        }
+      },
+      (error) => {
+        this.securityCheck=false
+
+      }
+    );
   }
 }
