6e22d00ec05ee742ca540450d11e7685590124d1
saifxo <saifii.7243@gmail.com>
2025-01-17 01:12:48 +0500

    removed verify domain api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor(private http: HttpClient, private router: Router) {}
-
-  securityCheck:boolean=false
-
-  ngOnInit(): void {
-    // Send an AJAX request to check domain validity
-    this.http.get<any>('https://flyeats.co.uk/verify-domain.php').subscribe(
-      (response) => {
-        if (response.status === 'true') {
-          this.securityCheck=true
-          // Domain is valid, continue with the website
-          console.log('Domain check passed');
-        } else {
-          // Domain is not valid, return blank page
-          this.securityCheck=false
-
-        }
-      },
-      (error) => {
-        this.securityCheck=false
-
-      }
-    );
-  }
+export class AppComponent  {
+  constructor() {}
+ 
 }
-``
